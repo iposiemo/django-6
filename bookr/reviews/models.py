a8421ec86bde66c1b3f3aa935deb98e0a2b2f602
iposiemo <paulnyabaro1@gmail.com>
2026-04-25 07:20:39 +0300

    Working on review models

--- bookr/reviews/models.py
+++ bookr/reviews/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Publisher(models.Model):
+    name = models.CharField(max_length=50, help_text="The name of the publisher")
+    website = models.URLField(blank=True, help_text="The website of the publisher")
+    email = models.EmailField(blank=True, help_text="The publisher's email address")
